49f6bdd3b1c0244a20ebdcc1305763f226f9dac8
Daniele Albanese <daniele.albanese98@mail.com>
2020-04-18 16:36:51 +0200

    Added relay controller to divide led strip in different sectors

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,11 @@ int color[] = {0, 0, 0};
 
 /*********************************************************************/
 
+int relay[] = {0, 4, 5, 16};
+bool status_relay[] = {false, false, false, false};
+
+/*********************************************************************/
+
 /**
  * Send response
  * 
@@ -113,6 +118,61 @@ void set_color(int red, int green, int blue) {
 /*********************************************************************/
 
 /**
+ * Size of relay array
+ * 
+ * @return number of element in array
+ */
+int get_relay_size() {
+    return sizeof(relay) / sizeof(relay[0]);
+}
+
+/**
+ * Initialization of relay pins
+ * 
+ * @param relay_size relay number
+ */
+void initRelay(int relay_size) {
+    for (int i = 0; i < relay_size; i++) {
+        pinMode(relay[i], OUTPUT);
+        digitalWrite(relay[i], HIGH);
+    }
+}
+
+/**
+ * Check if the relay pin received is correct
+ * 
+ * @param num relay pin
+ * @return "true" if is correct | "false" if is incorrect
+ */
+bool is_valid_relay_pin(int num) {
+    if (num >= 0 && num <= get_relay_size() - 1 && num != NULL) {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * Turn on a specific relay pin
+ * 
+ * @param pin pin of relay pin
+ */
+void turnOn(int pin) {
+    digitalWrite(pin, LOW);
+}
+
+/**
+ * Turn off a specific relay pin
+ * 
+ * @param pin Pin of relay pin
+ */
+void turnOff(int pin) {
+    digitalWrite(pin, HIGH);
+}
+
+/*********************************************************************/
+
+/**
  * Receives the value of the colors and sets them
  */
 void checkArgs() {
@@ -166,10 +226,79 @@ void getStatus() {
 
 /*********************************************************************/
 
+/**
+ * Turn on/off a specific relay by parameters
+ */
+void checkRelayArgs() {
+    int html_code;
+    String message = "";
+
+    DynamicJsonDocument body(1024);
+
+    if (server.hasArg("plain") == false) {
+        html_code = 400;
+        message = "Body not found";
+    } else {
+        deserializeJson(body, server.arg("plain"));
+
+        String action = body["action"];
+        int pin = body["pin"];
+        pin -= 1;
+
+        if (action == "on" || action == "off") {
+            if (is_valid_relay_pin(body["pin"])) {
+                html_code = 200;
+                message = "Successful action";
+
+                if (action == "on") {
+                    turnOn(relay[pin]);
+                    status_relay[pin] = true;
+                } else {
+                    turnOff(relay[pin]);
+                    status_relay[pin] = false;
+                }
+            } else {
+                html_code = 400;
+                message = "Relay number not found";
+            }
+        } else {
+            html_code = 400;
+            message = "Action not found";
+        }
+    }
+
+    sendResponse(html_code, message);
+}
+
+/*********************************************************************/
+
+/**
+ * Send the status of relays
+ */
+void getRelayStatus() {
+    int html_code = 200;
+    String status;
+    DynamicJsonDocument doc(1024);
+
+    for (int i = 0; i < get_relay_size(); i++) {
+        int num = i + 1;
+        String name = "relay_" + String(num);
+
+        doc[name] = status_relay[i];
+    }
+
+    serializeJson(doc, status);
+
+    sendResponse(html_code, status);
+}
+
+/*********************************************************************/
+
 void setup() {
     Serial.begin(9600);
 
     init_rgb();
+    initRelay(get_relay_size());
 
     // WiFiManager
     WiFiManager wifiManager;
@@ -190,8 +319,11 @@ void setup() {
 
     Serial.println("Connected");
 
-    server.on("/", checkArgs);
-    server.on("/status", getStatus);
+    server.on("/rgb", checkArgs);
+    server.on("/rgb/status", getStatus);
+
+    server.on("/ps", checkRelayArgs);
+    server.on("/ps/status", getRelayStatus);
 
     server.begin();
 }
